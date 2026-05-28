ffa43b041bac2b695b0cbc91e6aff859026d465e
Shivkumarrathod <shivarathod244@gmail.com>
2026-05-28 23:01:11 +0530

    fix: add /ping endpoint to respond with 'Pong!'

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Pong!')
 })
+app.get('/ping', (req, res) => {
+    res.send('Pong!')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
