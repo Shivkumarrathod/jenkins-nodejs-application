2256e53b41f320c67b554a28f5d0c0e2e1a43841
Shivkumarrathod <shivarathod244@gmail.com>
2026-05-29 19:56:11 +0530

    fix: add dynamic ID endpoint to respond with requested ID

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
 app.get('/ping', (req, res) => {
     res.send('Pong!')
 })
+app.get("/:id", (req, res) => {
+    const id = req.params.id
+    res.send(`You requested ID: ${id}`)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
