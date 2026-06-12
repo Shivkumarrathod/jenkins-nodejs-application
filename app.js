dffb9f49db4381556a82bc72acdaac21bd3e751b
Shivkumarrathod <shivarathod244@gmail.com>
2026-06-12 18:17:14 +0530

    fix: add search endpoint to respond with queried search term

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/:id", (req, res) => {
     const id = req.params.id
     res.send(`You requested ID: ${id}`)
 })
+app.get("/search/:query", (req,res)=>{
+    const query = req.params.query
+    res.send(`You searched for: ${query}`)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
